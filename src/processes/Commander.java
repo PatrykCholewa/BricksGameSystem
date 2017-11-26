4303130255d998bd053be91ad5f2868b74d4a74d
Patryk <patr.cholewa@gmail.com>
2017-11-26 17:45:14 +0100

    add Commander ( with some Unsupported Operations )

--- src/processes/Commander.java
+++ src/processes/Commander.java
@@ -1,6 +1,7 @@
 package processes;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
@@ -8,11 +9,43 @@ class Commander {
 
     private Scanner witnessStdout;
     private BufferedWriter witnessStdin;
+    private Witness witness;
 
     Commander( Witness witness ){
 
+        this.witness = witness;
         witnessStdout = new Scanner( witness.getInputStream() );
         witnessStdin = new BufferedWriter( new OutputStreamWriter( witness.getOutputStream() ) );
+
+    }
+
+    String getWitnessNick(){
+        return witness.getNick();
+    }
+
+    Boolean hasOutput(){
+        throw new UnsupportedOperationException();
+//        return witnessStdout.hasNext();
+    }
+
+    String getOutputLine(){
+        return witnessStdout.nextLine();
+    }
+
+    void tellInputLine( String line) throws IOException{
+        throw new UnsupportedOperationException();
+//        witnessStdin.write(line);
+    }
+
+    void killWitness(){
+/*
+        try {
+            tellInputLine("STOP");
+        } catch ( IOException e ){
+            ;
+        }
+*/
+        witness.destroy();
 
     }
 

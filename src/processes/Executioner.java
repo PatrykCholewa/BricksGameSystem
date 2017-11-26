e355659361dfe53001488adbeb79451cafbf3800
Patryk <patr.cholewa@gmail.com>
2017-11-26 19:00:13 +0100

    add Executioner

---
+++ src/processes/Executioner.java
@@ -0,0 +1,40 @@
+package processes;
+
+import java.io.File;
+import java.nio.file.NotDirectoryException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Executioner implements Iterator{
+
+    private File mainDir;
+    private ArrayList<File> playerDirs;
+
+    Executioner( File mainDir ) throws NotDirectoryException {
+
+        if( !mainDir.isDirectory() ){
+            throw new NotDirectoryException( "\"" + mainDir.getPath() + "\" is not a directory!" );
+        }
+
+        this.mainDir = mainDir;
+
+        this.playerDirs = new ArrayList<>();
+        playerDirs.addAll(Arrays.asList(mainDir.listFiles()));
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !playerDirs.isEmpty();
+    }
+
+    @Override
+    public File next() {
+        File tmp = new File( playerDirs.get(0).getPath() );
+        playerDirs.remove(0);
+        return tmp;
+    }
+
+
+}

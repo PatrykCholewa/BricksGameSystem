62bed8fd48a4c15d713390bf3ada3acd89642898
PAwel_Z <pawelzych97@gmail.com>
2017-12-01 01:49:54 +0100

    added all menu listeners, added some dialogs, file chooser

--- src/graphics/Controller.java
+++ src/graphics/Controller.java
@@ -4,19 +4,24 @@ import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.StrokeLineCap;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 import java.awt.*;
-import java.util.IllegalFormatException;
+import java.io.File;
+import java.util.Optional;
 
 public class Controller {
     int scale=10;
-    int n;
+    int size;
     int i =1;
 
     @FXML
@@ -30,10 +35,6 @@ public class Controller {
     @FXML
     Label nickname2 = new Label();
     @FXML
-    TextField tablesizeLabel = new TextField();
-    @FXML
-    Button tablesizeButton = new Button();
-    @FXML
     Canvas board = new Canvas();
     @FXML
     AnchorPane boardPane = new AnchorPane();
@@ -41,19 +42,116 @@ public class Controller {
     Label statusLabel = new Label();
     @FXML
     TextArea logText = new TextArea();
-
+    @FXML
+    MenuItem setSize = new MenuItem();
+    @FXML
+    MenuItem selectLog = new MenuItem();
+    @FXML
+    MenuItem displayLog = new MenuItem();
+    @FXML
+    MenuItem close = new MenuItem();
+    @FXML
+    MenuItem randomBarrier = new MenuItem();
+    @FXML
+    MenuItem manualBarrier = new MenuItem();
+    @FXML
+    MenuItem tournament = new MenuItem();
+    @FXML
+    MenuItem duel = new MenuItem();
+    @FXML
+    MenuItem about = new MenuItem();
 
     @FXML
-    void tablesizePressed(){
-        try {
-            n = Integer.parseInt(tablesizeLabel.getText());
-        } catch (IllegalFormatException e) {
-            statusLabel.setText("Wrong Number");
-        }
+    void aboutPressed() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText("Autorzy:");
+        alert.setContentText("Patryk \nPaweł");
 
-        status.setText("Size: "+n);
-        clearBoard();
-        drawNet();
+        alert.showAndWait();
+    }
+
+    @FXML
+    void closePressed() {
+        System.exit(0);
+    }
+
+    @FXML
+    void tournamentPressed() {
+        showWIP();
+    }
+
+    @FXML
+    void duelPressed() {
+        showWIP();
+    }
+
+    @FXML
+    void manualBarrierPressed() {
+        showWIP();
+    }
+
+    @FXML
+    void randomBarrierPressed() {
+        showWIP();
+    }
+
+    @FXML
+    void displayLogPressed() {
+        System.out.println(selectLogFile(true,false).toString());
+    }
+
+    @FXML
+    void selectLogPressed() {
+        System.out.println((selectLogFile(false,true)).toString());
+    }
+
+    File selectLogFile(boolean read,boolean write){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select log file");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File selectedFile = fileChooser.showOpenDialog((Stage) boardPane.getScene().getWindow());
+        if (selectedFile != null) {
+            return selectedFile;
+        }
+        return null;
+    }
+
+    void showWIP(){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Warning");
+        alert.setHeaderText("Work in progress");
+        alert.setContentText("");
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    void setSizePressed(){
+        initialize();
+        TextInputDialog dialog = new TextInputDialog("17");
+        dialog.setTitle("Set board size");
+        dialog.setHeaderText("Set board size");
+        dialog.setContentText("size");
+
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            try {
+                size = Integer.parseInt(result.get());
+            } catch (NumberFormatException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(result.get()+" is not an number!!!");
+                alert.setContentText("");
+                alert.showAndWait();
+                statusLabel.setText("Wrong Number");
+            }
+            System.out.println("Size: " + result.get());
+            clearBoard();
+            drawNet();
+        }
     }
 
     @FXML
@@ -67,12 +165,12 @@ public class Controller {
         status.setText("Test draw");
         drawSingleCell(1,new Point(1,1),new Point(1,2));
         drawSingleCell(2,new Point(4,4),new Point(5,4));
-        drawSingleCell(0,new Point(Integer.parseInt(tablesizeLabel.getText())-1,Integer.parseInt(tablesizeLabel.getText())-1),new Point(0,0));
+        drawSingleCell(0,new Point(size-1,size-1),new Point(0,0));
 
     }
 
     ChangeListener<Number> boardPaneSizeListener = (observable, oldValue, newValue) ->
-        redrawNet();
+            redrawNet();
 
     void initialize(){
         boardPane.widthProperty().addListener(boardPaneSizeListener);
@@ -94,9 +192,9 @@ public class Controller {
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(1);
         gc.setLineCap(StrokeLineCap.SQUARE);
-        for (int i = 0; i<=n;i++) {
-            gc.strokeLine(snap(i*scale),0,snap(i*scale),scale*n);
-            gc.strokeLine(0,snap(i*scale),scale*n,snap(i*scale));
+        for (int i = 0; i<= size; i++) {
+            gc.strokeLine(snap(i*scale),0,snap(i*scale),scale* size);
+            gc.strokeLine(0,snap(i*scale),scale* size,snap(i*scale));
         }
     }
 
@@ -106,9 +204,9 @@ public class Controller {
         double width = boardPane.getWidth();
         double height = boardPane.getHeight();
         if (height < width) {
-            scale = (int)height/(n);
+            scale = (int)height/(size);
         } else {
-            scale = (int) width / (n);
+            scale = (int) width / (size);
         }
         System.out.println("scale = " + scale);
         if (scale < 3) {

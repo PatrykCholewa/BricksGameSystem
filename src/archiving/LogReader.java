54645e415632c905b69718b7a584ce78dc39764b
Patryk <patr.cholewa@gmail.com>
2017-12-07 23:47:06 +0100

    LogReader

---
+++ src/archiving/LogReader.java
@@ -0,0 +1,58 @@
+package archiving;
+
+import java.io.*;
+import java.net.ProtocolException;
+import java.util.ArrayList;
+
+/**
+ * @author Patryk Cholewa
+ */
+
+public class LogReader {
+
+    final int size;
+    final String startingPlayer;
+    final String followingPlayer;
+    final String initData;
+    private ArrayList<String> moves;
+    final String winner;
+    final String finishMessage;
+
+    LogReader( File infoFile ) throws FileNotFoundException, ProtocolException {
+
+            BufferedReader bufferedReader = new BufferedReader( new FileReader( infoFile ) );
+            String []stringList;
+            moves = new ArrayList<>();
+
+        try {
+
+            //INIT
+            String line = bufferedReader.readLine();
+            line = line.split( "\\|" )[0];
+            stringList = line.split( "#" );
+            size = Integer.valueOf( stringList[0] );
+            startingPlayer = stringList[1];
+            followingPlayer = stringList[2];
+            initData = bufferedReader.readLine();
+
+            //MOVES
+            while( !(line = bufferedReader.readLine() ).equals( "---" ) ){
+                line = line.split( ":" )[0];
+                line = line.substring( 0 , line.length() - 1 );
+                moves.add( line );
+            }
+
+            winner = bufferedReader.readLine();
+            finishMessage = bufferedReader.readLine();
+
+        } catch (IOException | IndexOutOfBoundsException e ) {
+            throw new ProtocolException( "Can't read logFile!" );
+        }
+
+    }
+
+    ArrayList<String> getMoves( ){
+        return moves;
+    }
+
+}

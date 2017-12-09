5e1817af52de41d1b036375eeb3e0efa73055372
Patryk <patr.cholewa@gmail.com>
2017-12-10 00:21:49 +0100

    DuelSaverTest

---
+++ test/tournaments/DuelSaverTest.java
@@ -0,0 +1,38 @@
+package tournaments;
+
+import org.junit.jupiter.api.Test;
+import testTools.FileComparator;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuelSaverTest {
+
+    private final static File dir = new File ( "./test/testFiles/tourFiles/duelListFiles" );
+    private final static File writeFile = new File( "./test/testFiles/tourFiles/duelListFiles/writeFile.txt" );
+    private final static File sample1 = new File( "./test/testFiles/tourFiles/duelListFiles/sample1.txt" );
+
+    @Test
+    void addDuel() throws IOException {
+
+        writeFile.createNewFile();
+
+        DuelSaver ds = new DuelSaver( writeFile );
+
+        ds.addDuel( 1 , "P1" , "P2" , "P2" );
+        ds.addDuel( 2 , "P1" , "P3" , "P3" );
+        ds.addDuel( 3 , "P2" , "P1" , "P2" );
+        ds.addDuel( 4 , "P2" , "P3" , "P2" );
+        ds.addDuel( 5 , "P3" , "P1" , "P1" );
+        ds.addDuel( 6 , "P3" , "P2" , "P2" );
+        ds.close();
+
+        FileComparator.compare( sample1 , writeFile );
+
+        writeFile.deleteOnExit();
+
+    }
+
+}
